f76ee0c7dfd738438d5bdec032e4ea47b6da759b
glenn kirchner <chintaka221@yahoo.com>
2016-10-16 12:54:21 -0700

    add showcompleted and searchtext reducers

---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,25 @@
+var expect = require('expect');
+var reducers = require('reducers');
+var df = require('deep-freeze-strict');
+
+describe('reducers', ()=>{
+	describe('searchTextReducer', ()=>{
+		it('should set searchText', ()=>{
+			var action = {
+				type: 'SET_SEARCH_TEXT',
+				searchText: 'something'
+			}
+			var res = reducers.searchTextReducer(df(''), df(action));
+			expect(res).toEqual(action.searchText);
+		});
+	});	
+
+	describe('toggleShowCompleted',()=>{
+		it('should toggle showCompleted', ()=>{
+			var action = {
+				type: 'TOGGLE_SHOW_COMPLETED',
+			}
+			var res = reducers.showCompletedReducer(df(false), df(action));
+		});
+	});
+});
--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,8 @@ module.exports = {
 			alias: {
 					//Main: 'app/components/main.jsx',
 					applicationStyles: 'app/styles/app.scss',
-					actions: 'app/actions/actions.jsx'
+					actions: 'app/actions/actions.jsx',
+					reducers: 'app/reducers/reducers.jsx'
 				},
 				extensions: ['', '.js', '.jsx']
 			},

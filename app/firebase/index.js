d8b03f50efc5058ef928d7c5d715ac28fbed681d
glenn kirchner <chintaka221@yahoo.com>
2016-10-23 14:09:08 -0700

    move firebase creds to env vars

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,11 @@ import firebase from 'firebase';
 
 try{
 var config = {
-    apiKey: "AIzaSyAD6Y5kMpDzGjkIm0zOmNjDF08GASgmcsg",
-    authDomain: "todoapp-497ef.firebaseapp.com",
-    databaseURL: "https://todoapp-497ef.firebaseio.com",
-    storageBucket: "todoapp-497ef.appspot.com",
-    messagingSenderId: "958180000673"
-  };
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGEBUCKET
+      };
   firebase.initializeApp(config);
 }catch(e){
 
